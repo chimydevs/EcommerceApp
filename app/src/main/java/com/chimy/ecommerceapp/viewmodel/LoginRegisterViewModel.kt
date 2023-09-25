366093bda986b7c478725daeb873576ce5c4ac8c
michimurri <joselo.okumura@gmail.com>
2023-09-25 17:47:19 -0300

    Firebase Auth

---
+++ app/src/main/java/com/chimy/ecommerceapp/viewmodel/LoginRegisterViewModel.kt
@@ -0,0 +1,32 @@
+package com.chimy.ecommerceapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.chimy.ecommerceapp.data.User
+import com.chimy.ecommerceapp.util.Resource
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginRegisterViewModel @Inject constructor(
+    private val fireBaseAuth: FirebaseAuth
+) : ViewModel() {
+
+    private val _register = MutableStateFlow<Resource<FirebaseUser>>(Resource.Loading())
+    val register: Flow<Resource<FirebaseUser>> = _register
+
+    fun createAccountWithEmailAndPassword(user: User, password: String) {
+        fireBaseAuth.createUserWithEmailAndPassword(user.email, password)
+            .addOnSuccessListener {
+                it.user?.let {
+                    _register.value = Resource.Success(it)
+                }
+            }.addOnFailureListener{
+                _register.value = Resource.Error(it.message.toString())
+            }
+    }
+
+}
