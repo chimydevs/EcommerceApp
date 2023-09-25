4d621374215a91d3bf8e161d6f62dac457f444dd
michimurri <joselo.okumura@gmail.com>
2023-09-25 20:23:58 -0300

    Create Account With Email

--- app/src/main/java/com/chimy/ecommerceapp/fragments/loginRegister/RegisterFragment.kt
+++ app/src/main/java/com/chimy/ecommerceapp/fragments/loginRegister/RegisterFragment.kt
@@ -1,7 +1,74 @@
 package com.chimy.ecommerceapp.fragments.loginRegister
 
+import android.nfc.Tag
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.chimy.ecommerceapp.R
+import com.chimy.ecommerceapp.data.User
+import com.chimy.ecommerceapp.databinding.FragmentRegisterBinding
+import com.chimy.ecommerceapp.util.Resource
+import com.chimy.ecommerceapp.viewmodel.RegisterViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
-class RegisterFragment: Fragment(R.layout.fragment_register) {
+private val TAG = "RegisterFragment"
+
+@AndroidEntryPoint
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    private lateinit var binding: FragmentRegisterBinding
+    private val viewModel by viewModels<RegisterViewModel>()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRegisterBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            registerButtom.setOnClickListener {
+                val user = User(
+                    idFirstName.text.toString().trim(),
+                    idLastName.text.toString().trim(),
+                    idEmailRegister.text.toString().trim(),
+                )
+                val password = idPasswordRegister.text.toString()
+                viewModel.createAccountWithEmailAndPassword(user, password)
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.register.collect {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.registerButtom.startAnimation()
+                    }
+
+                    is Resource.Success -> {
+                        Log.d("test", it.data.toString())
+                        binding.registerButtom.revertAnimation()
+                    }
+
+                    is Resource.Error -> {
+                        Log.e(TAG, it.message.toString())
+                        binding.registerButtom.revertAnimation()
+                    }
+
+                    else -> Unit
+                }
+
+            }
+        }
+    }
 }
--- app/src/main/java/com/chimy/ecommerceapp/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/chimy/ecommerceapp/viewmodel/RegisterViewModel.kt
@@ -8,17 +8,22 @@ import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginRegisterViewModel @Inject constructor(
+class RegisterViewModel @Inject constructor(
     private val fireBaseAuth: FirebaseAuth
 ) : ViewModel() {
 
-    private val _register = MutableStateFlow<Resource<FirebaseUser>>(Resource.Loading())
+    private val _register = MutableStateFlow<Resource<FirebaseUser>>(Resource.Unspecified())
     val register: Flow<Resource<FirebaseUser>> = _register
 
     fun createAccountWithEmailAndPassword(user: User, password: String) {
+        runBlocking {
+            _register.emit(Resource.Loading())
+        }
+
         fireBaseAuth.createUserWithEmailAndPassword(user.email, password)
             .addOnSuccessListener {
                 it.user?.let {
