8c79283767975a99586160f38dab398b717fd984
michimurri <joselo.okumura@gmail.com>
2023-09-27 06:42:27 -0300

    Login and Navigation

--- app/src/main/java/com/chimy/ecommerceapp/fragments/loginRegister/LoginFragment.kt
+++ app/src/main/java/com/chimy/ecommerceapp/fragments/loginRegister/LoginFragment.kt
@@ -1,7 +1,77 @@
 package com.chimy.ecommerceapp.fragments.loginRegister
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.chimy.ecommerceapp.R
+import com.chimy.ecommerceapp.activities.ShoopingActivity
+import com.chimy.ecommerceapp.databinding.FragmentLoginBinding
+import com.chimy.ecommerceapp.databinding.FragmentRegisterBinding
+import com.chimy.ecommerceapp.util.Resource
+import com.chimy.ecommerceapp.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
-class LoginFragment: Fragment(R.layout.fragment_login) {
+@AndroidEntryPoint
+class LoginFragment : Fragment(R.layout.fragment_login) {
+    private lateinit var binding: FragmentLoginBinding
+    private val viewModel by viewModels<LoginViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvRegisterNow.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.apply {
+            buttomLoginLogin.setOnClickListener {
+                val email = idEmailLogin.text.toString().trim()
+                val password = idPasswordLogin.text.toString()
+                viewModel.login(email, password)
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.login.collect {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.buttomLoginLogin.startAnimation()
+                    }
+
+                    is Resource.Success -> {
+                        binding.buttomLoginLogin.revertAnimation()
+                        Intent(requireActivity(), ShoopingActivity::class.java).also { intent ->
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                            startActivity(intent)
+                        }
+                    }
+
+                    is Resource.Error -> {
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                        binding.buttomLoginLogin.revertAnimation()
+                    }
+
+                    else -> Unit
+                }
+
+            }
+        }
+    }
 }
--- app/src/main/java/com/chimy/ecommerceapp/fragments/loginRegister/RegisterFragment.kt
+++ app/src/main/java/com/chimy/ecommerceapp/fragments/loginRegister/RegisterFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.chimy.ecommerceapp.R
 import com.chimy.ecommerceapp.data.User
 import com.chimy.ecommerceapp.databinding.FragmentRegisterBinding
@@ -38,6 +39,10 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.tvLoginNow.setOnClickListener{
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
 
         binding.apply {
             registerButtom.setOnClickListener {
