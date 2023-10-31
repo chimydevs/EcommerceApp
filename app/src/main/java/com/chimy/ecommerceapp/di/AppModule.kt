b36a3a8a693f8342db8ec220c69d047dff865f3b
michimurri <joselo.okumura@gmail.com>
2023-10-31 18:27:02 -0300

    ViewModel IF

--- app/src/main/java/com/chimy/ecommerceapp/di/AppModule.kt
+++ app/src/main/java/com/chimy/ecommerceapp/di/AppModule.kt
@@ -1,5 +1,8 @@
 package com.chimy.ecommerceapp.di
 
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import com.chimy.ecommerceapp.util.Constants.INTRODUCTION_SP
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -21,5 +24,9 @@ object AppModule {
     @Singleton
     fun provideFirebaseFirestoreDatabase() = Firebase.firestore
 
+    @Provides
+    fun provideIntroductionSP(
+        application: Application
+    ) = application.getSharedPreferences(INTRODUCTION_SP, MODE_PRIVATE)
 
 }
