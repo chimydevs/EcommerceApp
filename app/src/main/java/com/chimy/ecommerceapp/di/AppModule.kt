1f514d129ba873b07c59363b968cafb009cfbc2f
michimurri <joselo.okumura@gmail.com>
2023-11-06 21:26:12 -0300

    Add to Cart

--- app/src/main/java/com/chimy/ecommerceapp/di/AppModule.kt
+++ app/src/main/java/com/chimy/ecommerceapp/di/AppModule.kt
@@ -2,8 +2,10 @@ package com.chimy.ecommerceapp.di
 
 import android.app.Application
 import android.content.Context.MODE_PRIVATE
+import com.chimy.ecommerceapp.firebase.FirebaseCommon
 import com.chimy.ecommerceapp.util.Constants.INTRODUCTION_SP
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.Module
@@ -28,5 +30,12 @@ object AppModule {
     fun provideIntroductionSP(
         application: Application
     ) = application.getSharedPreferences(INTRODUCTION_SP, MODE_PRIVATE)
+
+    @Provides
+    @Singleton
+    fun provideFirebaseCommon(
+        firebaseAuth: FirebaseAuth,
+        firestore: FirebaseFirestore,
+    ) = FirebaseCommon(firestore,firebaseAuth)
 
 }
--- app/src/main/java/com/chimy/ecommerceapp/fragments/shopping/ProductDetailsFragment.kt
+++ app/src/main/java/com/chimy/ecommerceapp/fragments/shopping/ProductDetailsFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,10 +17,16 @@ import com.chimy.ecommerceapp.activities.ShoppingActivity
 import com.chimy.ecommerceapp.adapters.ColorsAdapter
 import com.chimy.ecommerceapp.adapters.SizesAdapters
 import com.chimy.ecommerceapp.adapters.ViewPager2Images
+import com.chimy.ecommerceapp.data.CartProduct
 import com.chimy.ecommerceapp.databinding.FragmentProductDetailsBinding
+import com.chimy.ecommerceapp.util.Resource
 import com.chimy.ecommerceapp.util.hideBottomNavigationView
+import com.chimy.ecommerceapp.viewmodel.DetailsViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
 class ProductDetailsFragment : Fragment() {
 
     private val args by navArgs<ProductDetailsFragmentArgs>()
@@ -25,6 +34,9 @@ class ProductDetailsFragment : Fragment() {
     private val viewPagerAdapter by lazy { ViewPager2Images() }
     private val sizesAdapter by lazy { SizesAdapters() }
     private val colorsAdapter by lazy { ColorsAdapter() }
+    private var selectedColor: Int? = null
+    private var selectedSize: String? = null
+    private val viewModel by viewModels<DetailsViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,6 +59,37 @@ class ProductDetailsFragment : Fragment() {
 
         binding.imageClose.setOnClickListener{
             findNavController().navigateUp()
+        }
+
+        sizesAdapter.onItemClick = {
+            selectedSize = it
+        }
+
+        colorsAdapter.onItemClick = {
+            selectedColor = it
+        }
+
+        binding.buttomAddToCart.setOnClickListener{
+            viewModel.addUpdateProductInCart(CartProduct(product, 1,selectedColor,selectedSize))
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.addToCart.collectLatest {
+                when(it){
+                    is Resource.Loading -> {
+                        binding.buttomAddToCart.startAnimation()
+                    }
+                    is Resource.Success -> {
+                        binding.buttomAddToCart.revertAnimation()
+                        binding.buttomAddToCart.setBackgroundColor(resources.getColor(R.color.black))
+                    }
+                    is Resource.Error -> {
+                        binding.buttomAddToCart.stopAnimation()
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                    }
+                    else -> Unit
+                }
+            }
         }
 
         binding.apply {
