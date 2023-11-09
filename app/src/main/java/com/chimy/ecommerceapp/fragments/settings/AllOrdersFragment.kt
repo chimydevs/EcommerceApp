2873e43de44cd0331342d0f6d49b38fa0643bb89
michimurri <joselo.okumura@gmail.com>
2023-11-09 18:54:15 -0300

    Order Details

--- app/src/main/java/com/chimy/ecommerceapp/fragments/settings/AllOrdersFragment.kt
+++ app/src/main/java/com/chimy/ecommerceapp/fragments/settings/AllOrdersFragment.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.chimy.ecommerceapp.adapters.AllOrdersAdapter
@@ -58,6 +59,12 @@ class AllOrdersFragment: Fragment() {
                 }
             }
         }
+
+        ordersAdapter.onClick = {
+            val action = AllOrdersFragmentDirections.actionOrdersFragmentToOrderDetailFragment(it)
+            findNavController().navigate(action)
+        }
+
     }
 
     private fun setupOrdersRv() {
